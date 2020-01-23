8a5c5290be998138f71d716774ef1a849f1f8962
Seyyed Mehdi Hassanpour <m.hassanpour@mahsan.co>
2020-01-23 12:09:27 +0330

    add vue-typed-mixins

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app" @click="show1()">
+    <img alt="Vue logo" src="./assets/logo.png" @click="show2()">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import mixins from "vue-typed-mixins";
+  import HelloWorld from './components/HelloWorld.vue';
+  import {test} from "./mixin";
+
+  export default mixins(test).extend({
+    name: 'app',
+    data() {
+      return {
+        testData: this.resetData()
+      }
+    },
+    components: {
+      HelloWorld
+    },
+    methods: {
+      show2() {
+        alert("show2")
+      },
+      show3(){
+        alert("show3");
+        this.testData = this.resetData();
+        this.show2();
+      }
+    }
+  });
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
